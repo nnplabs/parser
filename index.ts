6ed89507635bc84ecb2ef58c5c6f0bcb2bd70bf2
gaurav <gaurav.dhalla@leapwallet.io>
2022-11-07 23:12:53 +0530

    rabbitmq setup

--- index.ts
+++ index.ts
@@ -1,33 +1,65 @@
+import { PrismaClient } from '@prisma/client';
 import { startStream, types } from 'near-lake-framework';
 import { transactionParser } from './parser/parser';
+import { RabbitMqConnection } from './rabbitMQ/setup';
 
 const lakeConfig: types.LakeConfig = {
   s3BucketName: "near-lake-data-mainnet",
   s3RegionName: "eu-central-1",
-  startBlockHeight: 77072188,
+  startBlockHeight: 70878531 ,
 };
 
 class NotificationService  {
-    async handleStreamerMessage(
+    private rabbitMqConnection: RabbitMqConnection;
+    private prisma: PrismaClient;
+
+    constructor(_rabbitMqConnection: RabbitMqConnection) {
+        this.rabbitMqConnection = _rabbitMqConnection;
+        this.prisma = new PrismaClient();
+    }
+
+    handleStreamerMessage = async(
         streamerMessage: types.StreamerMessage
-      ): Promise<void> {
+      ): Promise<void> => {
           const { block, shards } = streamerMessage;
           const blockHeight = block.header.height;
           const timestamp = new Date(block.header.timestamp/1000);
 
+          // Parse transactions
           // Do something with the block and shards
           const transactions = shards.map((shard) => shard.chunk?.transactions ? shard.chunk?.transactions : []).reduce((a, b) => a.concat(b), []);
           if (transactions && transactions.length > 0) {
-              transactions.forEach((tx) => {
-                transactionParser(tx, timestamp)
+              transactions.forEach(async (tx) => {
+                await transactionParser(tx, timestamp, this.rabbitMqConnection)
               });
+              
           }
-          console.log(`Block ${blockHeight} processed`);
+
+          // this.prisma.blockHeight.upsert({
+          //   where: {
+          //       network: "MAINNET"
+          //   },
+          //   update: {
+          //     lastProcessedBlock: blockHeight,
+          //   },
+          //   create: {
+          //     lastProcessedBlock: blockHeight,
+          //     network: "MAINNET"
+          //   }
+          // })
       }
 }
 
 
 (async () => {
-    const notificationService = new NotificationService();
+    const rabbitMqConnection = new RabbitMqConnection();
+    // await rabbitMqConnection.setUp()
+    // await rabbitMqConnection.channel.consume("nnp-msg-queue", (msg) => console.log("listening", msg?.content.toString()));
+
+    // const prisma = new PrismaClient();
+    // const lastProcessedBlock = await prisma.blockHeight.findFirst({ where: { network: "MAINNET" } });
+    const notificationService = new NotificationService(rabbitMqConnection);
+
+    // lakeConfig.startBlockHeight = lastProcessedBlock?.lastProcessedBlock || 77072188;
     await startStream(lakeConfig, notificationService.handleStreamerMessage);
 })();
---
+++ rabbitMQ/setup.ts
@@ -0,0 +1,74 @@
+import amqplib from 'amqplib';
+
+export class RabbitMqConnection {
+    private connection: amqplib.Connection | undefined;
+    private _channel: amqplib.Channel | undefined;
+
+    get channel(): amqplib.Channel {
+        return this._channel!;
+    }
+
+    async setUp(): Promise<void> {
+        await this.setUpConnection();
+        await this.setUpChannel();
+        await this.channel.assertQueue('nnp-msg-queue');
+    }
+
+    private async setUpConnection(): Promise<void> {
+        try {
+            console.log('Connecting to RabbitMQ...');
+            this.connection = await amqplib.connect('amqps://fvghnida:hvQ8B8sshWkQcWEInczR3_qRaiv8LMV6@puffin.rmq2.cloudamqp.com/fvghnida');
+        } catch (err: any) {
+            console.log(`Failed to connect: ${err}`);
+            throw new Error(err);
+        } finally {
+            console.log('Connected to RabbitMQ');
+        }
+        this.connection.on('error', async (err) => {
+            console.log(`Disconnecting due to a connection error: ${err}`);
+            await this.disconnect();
+        });
+    }
+
+    private async setUpChannel(): Promise<void> {
+        try {
+            this._channel = await this.connection!.createChannel();
+        } catch (err) {
+            console.log(`Disconnecting due to channel creation failing: ${err}`);
+            await this.disconnect();
+            throw new Error(err as string);
+        }
+        this.channel.on('error', async (err) => {
+            console.log(`Disconnecting due to a channel error: ${err}`);
+            await this.disconnect();
+        });
+    }
+
+    async disconnect(): Promise<boolean> {
+        const isClosed = await this.closeChannel();
+        if (!isClosed) return false;
+        return await this.closeConnection();
+    }
+
+    private async closeChannel(): Promise<boolean> {
+        try {
+            await this._channel?.close();
+        } catch (err) {
+            console.error(`Failed to close channel: ${err}`);
+            return false;
+        }
+        this._channel = undefined;
+        return true;
+    }
+
+    private async closeConnection(): Promise<boolean> {
+        try {
+            await this.connection?.close();
+        } catch (err) {
+            console.log(`Failed to close connection: ${err}`);
+            return false;
+        }
+        this.connection = undefined;
+        return true;
+    }
+}
