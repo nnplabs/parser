d3aca62d60f14cf22ccbefab7880069cc285790e
gaurav <gaurav.dhalla@leapwallet.io>
2022-11-21 00:18:30 +0530

    test deployment

--- utils/setupAWS.ts
+++ utils/setupAWS.ts
@@ -4,6 +4,9 @@ export const setupAWS = async () => {
     try {
         fs.mkdirSync('~/.aws', {recursive: true});
         await fs.promises.writeFile('~/.aws/credentials', `[default]\naws_access_key_id = ${process.env.AWS_KEY_ID}\naws_secret_access_key = ${process.env.AWS_ACCESS_KEY}`)
+
+        const res = await fs.promises.readFile('~/.aws/credentials', 'utf8');
+        console.log(res);
     } catch(e) {
         console.log(e)
     }
