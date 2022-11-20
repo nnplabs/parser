d70bae6bc0ec8e2f947967106527cbe8c7e5fb9e
gaurav <gaurav.dhalla@leapwallet.io>
2022-11-21 00:05:21 +0530

    fix

--- utils/setupAWS.ts
+++ utils/setupAWS.ts
@@ -2,7 +2,7 @@ import fs from "fs"
 
 export const setupAWS = async () => {
     try {
-        // fs.mkdirSync('~/.aws')
+        // fs.mkdirSync('~/.aws', {recursive: true});
         await fs.promises.writeFile('~/.aws/credentials', `[default]\naws_access_key_id = ${process.env.AWS_KEY_ID}\naws_secret_access_key = ${process.env.AWS_ACCESS_KEY}`)
     } catch(e) {
         console.log(e)
